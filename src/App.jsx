552318fedb7a82a6a2ecda45b9ee96283be3e0ff
Andres-1197 <andresfernandez.1197@gmail.com>
2026-07-10 09:01:14 -0300

    vista de a cuatro

--- src/App.jsx
+++ src/App.jsx
@@ -325,7 +325,7 @@ const Specialties = () => {
                     <h2 className="text-4xl">Servicios Médicos</h2>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                     {list.map((item, idx) => (
                         <motion.div
                             key={idx}
